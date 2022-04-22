da3d66b23e14ed1efcaf8825b45e6d1486c103d1
Анна Никулина <anna.nikulina89@mail.ru>
2022-04-22 21:03:45 +0300

    Bonus Mile

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 400; // Цена билета
+        int conversion = 20;      // Количество рублей для одной бонусной мили
+
+        int bonusMile = ticketPrice / 20;
+        System.out.println(bonusMile);
+    }
+}
